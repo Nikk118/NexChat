ebca9bee69b0b250a158e41a31a776add252bef7
nikhil <nikhilyagik2004@gmail.com>
2026-04-09 00:43:16 +0530

    url updated

--- frontend/app.py
+++ frontend/app.py
@@ -3,13 +3,10 @@ import streamlit as st
 import uuid
 import os 
 # CONFIG
-def get_backend_url():
-    try:
-        return st.secrets["BACKEND_URL"]
-    except Exception:
-        return os.getenv("BACKEND_URL", "http://localhost:8000")
 
-BACKEND_URL = get_backend_url()
+
+# BACKEND_URL = "http://localhost:8000"
+BACKEND_URL="https://nexchat-backend-6j4o.onrender.com"
 
 # UTILS
 def genrate_thread_id():
